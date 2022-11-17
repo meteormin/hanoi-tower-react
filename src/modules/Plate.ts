2d23dd5c65af9f00782a404e57d70c4702b2eece
SeongMin Yoo <42951596+miniyus@users.noreply.github.com>
2022-11-17 15:59:28 +0900

    fix: 사용하지 않는 프로퍼티 제거

--- src/modules/Plate.ts
+++ src/modules/Plate.ts
@@ -1,16 +1,12 @@
 import { Plate as PlateInterface } from '../types';
 
 class Plate implements PlateInterface {
-  private _bottom: PlateInterface | null;
   private _size: number;
   private _selected: boolean;
-  private _top: PlateInterface | null;
 
   constructor(size: number) {
     this._size = size;
     this._top = null;
-    this._bottom = null;
-    this._selected = false;
   }
 
   get size(): number {
@@ -19,22 +15,6 @@ class Plate implements PlateInterface {
 
   set size(value: number) {
     this._size = value;
-  }
-
-  get bottom(): PlateInterface | null {
-    return this._bottom;
-  }
-
-  set bottom(value: PlateInterface | null) {
-    this._bottom = value;
-  }
-
-  get top(): PlateInterface | null {
-    return this._top;
-  }
-
-  set top(value: PlateInterface | null) {
-    this._top = value;
   }
 
   get selected(): boolean {
